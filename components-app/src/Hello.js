8e4e5a189e99a7dfb07f8e7a1704f376e166659d
Emanuele Braco <emanuele.braco@gmail.com>
2022-05-27 18:53:16 +0200

    feature/components-07

--- components-app/src/Hello.js
+++ components-app/src/Hello.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React from 'react';
+import {Message} from './Message'
 
 export class Hello extends React.Component {
   render() {
@@ -6,6 +7,7 @@ export class Hello extends React.Component {
       <div>
         <h1>Hello,World!</h1>
         <p>What a beautifull day!</p>
+        <Message></Message>
       </div>
     );
   }

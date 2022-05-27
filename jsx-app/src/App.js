13c66332a54ea53357a4be06803baea96ec1156f
Emanuele Braco <emanuele.braco@gmail.com>
2022-05-27 15:56:30 +0200

    feature/JSX-05

--- jsx-app/src/App.js
+++ jsx-app/src/App.js
@@ -3,14 +3,21 @@ import './App.css';
 function App() {
   const hello= <h1>Hello</h1>
 
-
   const myFunction = (name) => <h2>Hello, {name}</h2>
+
+  const sum = (a,b) => {
+    return <h2>{a+b}</h2>
+  }
+  // const result= <h2>{sum(10,20)}</h2>
 
   return (
     <div className="App">
-      {hello}
+
+      {hello}  
       
       {myFunction('Mario')}
+
+      {sum(10,20)}
     </div>
   );
 }

bb20f451ca3d65f9cd15be4b73c2d7322885e692
Emanuele Braco <emanuele.braco@gmail.com>
2022-05-27 15:49:35 +0200

    feature/JSX-02

--- jsx-app/src/App.js
+++ jsx-app/src/App.js
@@ -2,9 +2,15 @@ import './App.css';
 
 function App() {
   const hello= <h1>Hello</h1>
+
+
+  const myFunction = (name) => <h2>Hello, {name}</h2>
+
   return (
     <div className="App">
       {hello}
+      
+      {myFunction('Mario')}
     </div>
   );
 }

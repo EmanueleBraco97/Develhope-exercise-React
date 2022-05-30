aa14fc2c6fbe39e8e1ceede7d6e3c71fd39db4c6
Emanuele Braco <emanuele.braco@gmail.com>
2022-05-30 16:22:11 +0200

    feature/props-02

--- esercizi-react/src/Welcome.js
+++ esercizi-react/src/Welcome.js
@@ -11,3 +11,15 @@ export class Welcome extends React.Component{
         )
     }
 }
+
+
+//N.B. PROPS-02:
+//-Se non viene passato alcun nome prop al componente...non succede nulla, non da nessun errore.
+
+//-possiamo impostare un valore di 'Default':
+
+Welcome.defaultProps = {
+    name : 'Emanuele'
+};
+
+//In questo caso, se non verrà passato alcun nome prop, in valore predefinato sarà sempre 'Emanuele'

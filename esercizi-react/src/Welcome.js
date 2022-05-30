643bcb6490c775d505e5255f5a4b6780ef03c18e
Emanuele Braco <emanuele.braco@gmail.com>
2022-05-30 12:59:24 +0200

    feature/props-04

--- esercizi-react/src/Welcome.js
+++ esercizi-react/src/Welcome.js
@@ -4,7 +4,7 @@ export class Welcome extends React.Component{
     render(){
         return(
             <div>
-                <p>Welcome, {this.props.name}</p>
+                <p><strong>Welcome, {this.props.name}</strong></p>
                 <p>La tua eta è : {this.props.age}</p>
             </div>
         )

ae0eb6936fe51af821b120715045d6dd06e250ac
Emanuele Braco <emanuele.braco@gmail.com>
2022-06-07 20:59:19 +0200

    feature/forms-06

--- esercizi-react/src/UncontrolLogin.js
+++ esercizi-react/src/UncontrolLogin.js
@@ -1,42 +1,39 @@
-import React from 'react';
+import React from "react";
 
 export class UncontrolLogin extends React.Component {
-    
-    handleFormSubmit = (event) => {
-        event.preventDefault()
 
-        const username = event.target.elements.username.value
-        const password = event.target.elements.password.value
-        const remember = event.target.elements.remember.checked
+  handleFormSubmit = (event) => {
+    event.preventDefault();
 
-        console.log(
-            username,
-            password,
-            remember
-        )
-    }
+    const username = event.target.elements.username.value;
+    const password = event.target.elements.password.value;
+    const remember = event.target.elements.remember.checked;
 
-    handleResetForm = () => {
-        const username = ''
-        const password = ''
-        const remember = ''
+    console.log("username:", username, "password:", password, remember);
+  };
 
-        console.log(username,password,remember)
-    }
+  handleResetForm = () => {
+    const username = "";
+    const password = "";
+    const remember = "";
 
+    console.log(username, password, remember);
+  };
 
-    render() { 
-        return (
-            <div>   
-                <form onSubmit={this.handleFormSubmit}>
-                    <input name = 'username'></input>
-                    <input name = 'password' type='password'></input>
-                    <input name = 'remember' type='checkbox'></input>
+  render() {
+    return (
+      <div>
+        <form ref={this._formRef} onSubmit={this.handleFormSubmit}>
+          <input name="username" autoFocus></input>
+          <input name="password" type="password"></input>
+          <input name="remember" type="checkbox"></input>
 
-                    <button type='submit'>Login</button>
-                    <button type='reset' onClick={this.handleResetForm}>Reset</button>
-                </form>
-            </div>
-        );
-    }
+          <button type="submit">Login</button>
+          <button type="reset" onClick={this.handleResetForm}>
+            Reset
+          </button>
+        </form>
+      </div>
+    );
+  }
 }

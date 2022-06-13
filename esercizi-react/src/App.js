5f982f65922afd55c266cca243bf7f34d4b7ab66
Emanuele Braco <emanuele.braco@gmail.com>
2022-06-13 18:14:21 +0200

    feature/function-components-02

--- esercizi-react/src/App.js
+++ esercizi-react/src/App.js
@@ -1,17 +1,11 @@
 import React from 'react';
-import {Welcome} from './Welcome';
+import {Sum} from './Sum'
 
-export class App extends React.Component {
-    onLogin = (state) => {
-        console.log(state)
-    }
-
-    render() { 
+export function App(){
         return (
             <div>
-                <Welcome name ='emanuele'></Welcome>
+                <Sum numbers={[7,9,13,27,14]}></Sum>
             </div>
         );
     }
-}
  

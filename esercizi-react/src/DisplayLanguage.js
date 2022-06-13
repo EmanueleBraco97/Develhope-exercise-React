513d09f33a13ca020360ff7c675ef179f28c619e
Emanuele Braco <emanuele.braco@gmail.com>
2022-06-13 12:42:04 +0200

    feature/context-02

--- esercizi-react/src/DisplayLanguage.js
+++ esercizi-react/src/DisplayLanguage.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import {LanguageContext} from './LanguageContext'
+
+
+const StringsLanguage = {
+    en: {
+        CURRENT_LANGUAGE: 'Current Language is: '
+    },
+    it: {
+        CURRENT_LANGUAGE: 'Lingua Attuale è: '
+    }
+}
 
 export class DisplayLanguage extends React.Component {
-    state = {  } 
     render() { 
         return (
-            <div></div>
+            <div>
+                <LanguageContext.Consumer>
+                    {(Language) =>{
+                        return <h1>{StringsLanguage[Language].CURRENT_LANGUAGE}</h1>
+                    }}
+                </LanguageContext.Consumer>
+            </div>
         );
     }
 }

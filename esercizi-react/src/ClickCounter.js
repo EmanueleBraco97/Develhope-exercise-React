4921ad8ded8bb486015152566c668532ae2776d1
Emanuele Braco <emanuele.braco@gmail.com>
2022-06-14 12:08:46 +0200

    feature/useSTATE-01

--- esercizi-react/src/ClickCounter.js
+++ esercizi-react/src/ClickCounter.js
@@ -1,27 +1,49 @@
-import React from 'react';
+import {useState} from 'react'
 
-export class ClickCounter extends React.Component {
-    state = { 
-        count: 0
-     } 
+export function ClickCounter({count = 0}){
+    const [counter, setCounter] = useState(count)
 
-     counterIncrement = () => {
-         this.setState((state) => {
-            return {
-                count: state.count + 1
-            }
-         })
-     }
-
-
-    render() { 
-        return (
-            <div>
-                <h3>Count: {this.state.count}</h3>
-                <button onClick = {this.counterIncrement}>Increment</button>
-            </div>
-            
-        );
+    function counterIncrement(){
+        setCounter(c => c+1)
     }
+    
+    return(
+        <div>
+            <h3>Count: {counter}</h3>
+            <button onClick={counterIncrement}>Increment</button>
+        </div>
+    )
 }
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react';
+// export class ClickCounter extends React.Component {
+//     state = { 
+//         count: 0
+//      } 
+//      counterIncrement = () => {
+//          this.setState((state) => {
+//             return {
+//                 count: state.count + 1
+//             }
+//          })
+//      }
+//     render() { 
+//         return (
+//             <div>
+//                 <h3>Count: {this.state.count}</h3>
+//                 <button onClick = {this.counterIncrement}>Increment</button>
+//             </div>        
+//         );
+//     }
+// }
  

ef07f830d422d1ce474c61ed7a2fd0dd4549ed06
Emanuele Braco <emanuele.braco@gmail.com>
2022-06-14 15:38:10 +0200

    feature/useSTATE-02

--- esercizi-react/src/Login.js
+++ esercizi-react/src/Login.js
@@ -1,53 +1,102 @@
-import React from 'react';
+import {useState} from 'react'
 
-export class Login extends React.Component {
-    state = { 
-        name:'',
-        password:'',
-        remember: false,
-     } 
+export function Login(){
+    const [data, setData] = useState({
+        username : '',
+        password : '',
+        remember : false
+    })
 
-     handleInputChange = (event) => {
-         const value = event.target.value
-         const name = event.target.name
+    function handleInputChange(event){
+        const {name, type, checked, value} = event.target
 
-        this.setState({
-            [name]: value
+        setData((data) => {
+            return{
+                ...data,
+                [name] : type === 'checkbox' ? checked : value
+            }
         })
-     }
-
-     handleCheckboxChange = (event) => {
-         const name = event.target.name
-         const checked = event.target.checked
-
-         this.setState({
-             [name]: checked
-         })
-     }
-
-     handleResetState = () => {
-         this.setState({
-            name:'',
-            password:'',
-            remember: false
-         })
-     }
-
-
-    render() { 
-        return (
-            <div>
-                <div>
-                    <button onClick={this.handleResetState}>Reset</button>
-                </div>
-                
-                <div>
-                    <button disabled = {!this.state.name || !this.state.password}>Login</button>
-                    <input type='text' name='name' value={this.state.name} onChange={this.handleInputChange}></input>
-                    <input type='password' name= 'password' value={this.state.password} onChange={this.handleInputChange}></input>
-                    <input type='checkbox' name='remember' checked= {this.state.remember} onChange= {this.handleCheckboxChange}></input>
-                </div>
-            </div>
-        );
     }
+    console.log(data)
+    return(
+        <form>
+            <input onChange={handleInputChange} name='username' value={data.username}></input>
+            <input onChange={handleInputChange} name='password' type='password' value={data.password}></input>
+            <input onChange={handleInputChange} name='remember' type='checkbox' value={data.remember}></input>
+        </form>
+    )
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react';
+// export class Login extends React.Component {
+//     state = { 
+//         name:'',
+//         password:'',
+//         remember: false,
+//      } 
+
+//      handleInputChange = (event) => {
+//          const value = event.target.value
+//          const name = event.target.name
+
+//         this.setState({
+//             [name]: value
+//         })
+//      }
+
+//      handleCheckboxChange = (event) => {
+//          const name = event.target.name
+//          const checked = event.target.checked
+
+//          this.setState({
+//              [name]: checked
+//          })
+//      }
+
+//      handleResetState = () => {
+//          this.setState({
+//             name:'',
+//             password:'',
+//             remember: false
+//          })
+//      }
+
+
+//     render() { 
+//         return (
+//             <div>
+//                 <div>
+//                     <button onClick={this.handleResetState}>Reset</button>
+//                 </div>
+                
+//                 <div>
+//                     <button disabled = {!this.state.name || !this.state.password}>Login</button>
+//                     <input type='text' name='name' value={this.state.name} onChange={this.handleInputChange}></input>
+//                     <input type='password' name= 'password' value={this.state.password} onChange={this.handleInputChange}></input>
+//                     <input type='checkbox' name='remember' checked= {this.state.remember} onChange= {this.handleCheckboxChange}></input>
+//                 </div>
+//             </div>
+//         );
+//     }
+// }

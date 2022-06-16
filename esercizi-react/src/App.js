5cb5cb38d8b291bd8ecd772e60fda920d64de155
Emanuele Braco <emanuele.braco@gmail.com>
2022-06-16 14:45:12 +0200

    feature/ use-effect-02

--- esercizi-react/src/App.js
+++ esercizi-react/src/App.js
@@ -4,7 +4,7 @@ import {Counter} from './Counter'
 export class App extends React.Component {
   render() { 
     return (
-      <Counter initialValue= {10} increment = {20} interval = {6000}></Counter>
+      <Counter initialValue= {10} increment = {20} interval = {4000}></Counter>
     );
   }
 }
--- esercizi-react/src/Counter.js
+++ esercizi-react/src/Counter.js
@@ -1,29 +1,68 @@
-import React from 'react';
+import {useEffect, useState} from 'react';
 import {CounterDisplay} from './CounterDisplay'
 
-export class Counter extends React.Component {
-    state = { 
-        count: this.props.initialValue,
-     } 
+export function Counter({initialValue, increment, interval}){
+    const [count, setCount] = useState(initialValue)
 
-     constructor(props){
-         super(props)
-
-        setInterval(() => {
-            this.setState((state) => {
-                return{
-                    count: state.count + this.props.increment,
-                }
-            })
-        }, this.props.interval);
-
-     }
-    render() { 
-        return (
-            <div>
-                <h1><CounterDisplay count = {this.state.count}></CounterDisplay></h1>
-            </div>
-            
-        );
-    }
+    useEffect(() => {
+        const newInterval = setInterval(() => {
+            setCount((count) => count + increment)
+        },interval);
+        return () => clearInterval(newInterval)
+    }, [count, increment, interval])
+    return (
+        <div>
+           <h1><CounterDisplay count={count}></CounterDisplay></h1> 
+        </div>
+    )
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// export class Counter extends React.Component {
+//     state = { 
+//         count: this.props.initialValue,
+//      } 
+
+//      constructor(props){
+//          super(props)
+
+//         setInterval(() => {
+//             this.setState((state) => {
+//                 return{
+//                     count: state.count + this.props.increment,
+//                 }
+//             })
+//         }, this.props.interval);
+
+//      }
+//     render() { 
+//         return (
+//             <div>
+//                 <h1><CounterDisplay count = {this.state.count}></CounterDisplay></h1>
+//             </div>
+            
+//         );
+//     }
+// }

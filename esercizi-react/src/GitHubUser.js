dea6104bc67b67f86a58e07326d96fd61726713e
Emanuele Braco <emanuele.braco@gmail.com>
2022-06-20 10:40:20 +0200

    feature/custom-hooks-03

--- esercizi-react/src/GitHubUser.js
+++ esercizi-react/src/GitHubUser.js
@@ -1,26 +1,14 @@
-import {useEffect, useState} from 'react'
+import { useGitHubUser } from "./useGitHubUser";
 
-export function GitHubUser({username}){
-    const [data, setData] = useState(null)
+export function GitHubUser({ username }) {
+  const { data } = useGitHubUser(username);
 
-    useEffect(() => {
-        fetch(`https://api.github.com/users/${username}`)
-            .then(response => {
-                return response.json()
-            })
-            .then(json => {
-                console.log(json)
-                setData(json)
-            })
-    }, [username])
-    
-
-    return (
-        <div>
-            {data && <h1>Id utente è: {data.id}</h1>}
-            {data && <h1>Nome utente è: {data.name}</h1>}
-            {data && <h1>Nome Login utente è: {data.login}</h1>}
-            {data && <h1>Città dell'utente è: {data.location}</h1>}
-        </div>
-    );
+  return (
+    <div>
+      {data && <h1>Id utente è: {data.id}</h1>}
+      {data && <h1>Nome utente è: {data.name}</h1>}
+      {data && <h1>Nome Login utente è: {data.login}</h1>}
+      {data && <h1>Città dell'utente è: {data.location}</h1>}
+    </div>
+  );
 }

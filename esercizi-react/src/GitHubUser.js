1816198b6f96b11b990f37aa349404254ad89c8a
Emanuele Braco <emanuele.braco@gmail.com>
2022-06-20 10:57:04 +0200

    feature/custom-hook-04

--- esercizi-react/src/GitHubUser.js
+++ esercizi-react/src/GitHubUser.js
@@ -1,14 +1,18 @@
 import { useGitHubUser } from "./useGitHubUser";
 
 export function GitHubUser({ username }) {
-  const { data } = useGitHubUser(username);
+  const { data, loading, error } = useGitHubUser(username);
 
   return (
     <div>
-      {data && <h1>Id utente è: {data.id}</h1>}
-      {data && <h1>Nome utente è: {data.name}</h1>}
-      {data && <h1>Nome Login utente è: {data.login}</h1>}
-      {data && <h1>Città dell'utente è: {data.location}</h1>}
+        {loading && <h1>Loading...</h1>}
+
+        {error && <h1>there are error</h1>}
+        
+        {data && <h1>Id utente è: {data.id}</h1>}
+        {data && <h1>Nome utente è: {data.name}</h1>}
+        {data && <h1>Nome Login utente è: {data.login}</h1>}
+        {data && <h1>Città dell'utente è: {data.location}</h1>}
     </div>
   );
 }

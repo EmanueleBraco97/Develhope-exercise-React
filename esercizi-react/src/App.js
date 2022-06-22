12c73b58b1998101fe3b24dc1636928337d97414
Emanuele Braco <emanuele.braco@gmail.com>
2022-06-22 17:09:51 +0200

    feature/react-router-04

--- esercizi-react/src/App.js
+++ esercizi-react/src/App.js
@@ -1,12 +1,14 @@
 import {Routes, Route} from 'react-router-dom';
 import { ClickCounter } from './ClickCounter';
 import {Welcome} from './Welcome'
+import { ShowGithubUser } from './ShowGithubUser';
 
 export function App(){
     return( 
             <Routes>
                 <Route path='/' element={<Welcome name='emanuele'></Welcome>}></Route>
                 <Route path='/ClickCounter' element={<ClickCounter></ClickCounter>}></Route>
+                <Route path='/:username' element={<ShowGithubUser></ShowGithubUser>}></Route>
             </Routes>     
     )
 }
---
+++ esercizi-react/src/GithubUser.js
@@ -0,0 +1,26 @@
+import {useEffect, useState} from 'react'
+
+export function GithubUser({username}){
+    const [data, setData] = useState(null)
+
+    useEffect(() => {
+        fetch(`https://api.github.com/users/${username}`)
+            .then(response => {
+                return response.json()
+            })
+            .then(json => {
+                console.log(json)
+                setData(json)
+            })
+    }, [username])
+    
+
+    return (
+        <div>
+            {data && <h1>Id utente è: {data.id}</h1>}
+            {data && <h1>Nome utente è: {data.name}</h1>}
+            {data && <h1>Nome Login utente è: {data.login}</h1>}
+            {data && <h1>Città dell'utente è: {data.location}</h1>}
+        </div>
+    );
+}

d90a31a8f7b4335406e414ede6b4d6f4b745a527
Emanuele Braco <emanuele.braco@gmail.com>
2022-06-22 19:17:18 +0200

    feature/react-router-04

--- esercizi-react/src/App.js
+++ esercizi-react/src/App.js
@@ -1,14 +1,20 @@
-import {Routes, Route} from 'react-router-dom';
-import { ClickCounter } from './ClickCounter';
-import {Welcome} from './Welcome'
-import { ShowGithubUser } from './ShowGithubUser';
+import { Routes, Route, Link } from "react-router-dom";
+import { Welcome } from "./Welcome";
+import { ClickCounter } from "./ClickCounter";
+import { ShowGithubUser } from "./ShowGithubUser";
 
-export function App(){
-    return( 
-            <Routes>
-                <Route path='/' element={<Welcome name='emanuele'></Welcome>}></Route>
-                <Route path='/ClickCounter' element={<ClickCounter></ClickCounter>}></Route>
-                <Route path='/:username' element={<ShowGithubUser></ShowGithubUser>}></Route>
-            </Routes>     
-    )
+export function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Welcome name="emanuele"></Welcome>}></Route>
+        <Route path="/ClickCounter" element={<ClickCounter></ClickCounter>}></Route>
+        <Route path="/users/:username" element={<ShowGithubUser></ShowGithubUser>}></Route>
+      </Routes>
+
+      <h1><Link to="/">Enter to Welcome</Link></h1>
+      <h2><Link to="/ClickCounter">Enter to ClickCounter</Link></h2>
+      <h3><Link to="/users/emanuelebraco97">Username</Link></h3>
+    </>
+  );
 }

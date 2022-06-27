8bac28335640d008cf37ad75276eccf5190c08c1
Emanuele Braco <emanuele.braco@gmail.com>
2022-06-27 18:31:31 +0200

    feature/SWR-03

--- esercizi-react/src/UseGitHubUsers.js
+++ esercizi-react/src/UseGitHubUsers.js
@@ -4,11 +4,18 @@ const fetcher = (url) => fetch(url).then((response) => response.json())
 const shouldFetch = (`https://api.github.com/users/`)
 
 export function UseGitHubUsers({username}) {
-  const { data, error } = useSWR(shouldFetch ? `https://api.github.com/users/${username}`  : null, fetcher)
+  const { data, error, mutate } = useSWR(shouldFetch ? `https://api.github.com/users/${username}`  : null, fetcher)
+
+  function refreshData(){
+    mutate()
+
+    console.log(data)
+  }
 
   
   return (
     <div>
+      <button onClick={refreshData}>Refresh</button>
       {!data && !error && <h3>Loading...</h3>}
       {error && <h3>There are an error</h3>}
       {data && !error && (

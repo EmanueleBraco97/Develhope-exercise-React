cc59f069585f83a7d851625e83491dafdfd237d4
Emanuele Braco <emanuele.braco@gmail.com>
2022-06-29 11:44:42 +0200

    feature/redux-01

---
+++ esercizi-react/src/state/CounterReducer.js
@@ -0,0 +1,45 @@
+const defaultState = 0;
+
+const INCREMENT = 'COUNTER@INCREMENT'
+const DECREMENT = 'COUNTER@DECREMENT'
+const RESET = 'COUNTER@RESET'
+
+export function incrementCounter(by = 1){
+    return{
+        type: INCREMENT,
+        paylod: by
+    }
+}
+
+export function decrementCounter(by = 1){
+    return{
+        type: DECREMENT,
+        paylod: by
+    }
+}
+
+export function resetCounter(){
+    return{
+        type: RESET
+    }
+}
+
+export function counterReducer(state = defaultState, action){
+    switch(action.type){
+        case INCREMENT: {
+            return state + action.paylod
+        }
+
+        case DECREMENT: {
+            return state - action.paylod
+        }
+
+        case RESET: {
+            return defaultState
+        }
+
+        default: {
+            return state
+        }
+    }
+}

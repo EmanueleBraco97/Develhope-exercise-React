8e621168687447f9099b9967c270d907b6873ee6
Emanuele Braco <emanuele.braco@gmail.com>
2022-06-29 12:02:17 +0200

    feature/redux-04

--- esercizi-react/src/index.js
+++ esercizi-react/src/index.js
@@ -2,8 +2,14 @@ import ReactDom from 'react-dom/client'
 import { App } from './App'
 import { incrementCounter } from './state/CounterReducer'
 import {store} from './state/Store'
+import {add, remove, edit} from './state/TodosReducer'
 
 store.dispatch(incrementCounter(10))
+
+store.dispatch(add({id: 1, title: 'FRONTEND', completed: true}))
+store.dispatch(add({id: 2, title: 'BACKEND', completed: false}))
+store.dispatch(edit(2, {completed: true}))
+store.dispatch(remove(1))
 
 
 const container = document.querySelector('#root')
--- esercizi-react/src/state/Store.js
+++ esercizi-react/src/state/Store.js
@@ -1,8 +1,10 @@
 import {combineReducer, createStore} from 'redux'
 import {counterReducer} from './CounterReducer'
+import { todoReducer } from './TodosReducer'
 
 const rootReducer = combineReducer({
     counter : counterReducer,
+    todos : todoReducer
 })
 
 export const store = createStore(rootReducer)
